c1f707a152c5e3390122f0683245c809e559f39f
Programacion315 <jorgeluishuergo88@gmail.com>
2023-03-29 16:26:11 -0500

    code optimization

--- main.py
+++ main.py
@@ -1,47 +1,55 @@
 import csv
 
-print("Chekclist automation")
-print("")
-print("Ing J.Luis Huergo")
-print("")
+print('\n\n\n')
+print('▄████▄   ██░ ██ ▓█████  ▄████▄   ██ ▄█▀ ██▓     ██▓  ██████ ▄▄▄█████▓    ▄▄▄       █    ██ ▄▄▄█████▓ ▒█████   ███▄ ▄███▓ ▄▄▄     ▄▄▄█████▓ ██▓ ▒█████   ███▄    █  ')
+print('▒██▀ ▀█  ▓██░ ██▒▓█   ▀ ▒██▀ ▀█   ██▄█▒ ▓██▒    ▓██▒▒██    ▒ ▓  ██▒ ▓▒   ▒████▄     ██  ▓██▒▓  ██▒ ▓▒▒██▒  ██▒▓██▒▀█▀ ██▒▒████▄   ▓  ██▒ ▓▒▓██▒▒██▒  ██▒ ██ ▀█   █ ')
+print('▒▓█    ▄ ▒██▀▀██░▒███   ▒▓█    ▄ ▓███▄░ ▒██░    ▒██▒░ ▓██▄   ▒ ▓██░ ▒░   ▒██  ▀█▄  ▓██  ▒██░▒ ▓██░ ▒░▒██░  ██▒▓██    ▓██░▒██  ▀█▄ ▒ ▓██░ ▒░▒██▒▒██░  ██▒▓██  ▀█ ██▒')
+print('▒▓▓▄ ▄██▒░▓█ ░██ ▒▓█  ▄ ▒▓▓▄ ▄██▒▓██ █▄ ▒██░    ░██░  ▒   ██▒░ ▓██▓ ░    ░██▄▄▄▄██ ▓▓█  ░██░░ ▓██▓ ░ ▒██   ██░▒██    ▒██ ░██▄▄▄▄██░ ▓██▓ ░ ░██░▒██   ██░▓██▒  ▐▌██▒')
+print('▒ ▓███▀ ░░▓█▒░██▓░▒████▒▒ ▓███▀ ░▒██▒ █▄░██████▒░██░▒██████▒▒  ▒██▒ ░     ▓█   ▓██▒▒▒█████▓   ▒██▒ ░ ░ ████▓▒░▒██▒   ░██▒ ▓█   ▓██▒ ▒██▒ ░ ░██░░ ████▓▒░▒██░   ▓██░')
+print('░ ░▒ ▒  ░ ▒ ░░▒░▒░░ ▒░ ░░ ░▒ ▒  ░▒ ▒▒ ▓▒░ ▒░▓  ░░▓  ▒ ▒▓▒ ▒ ░  ▒ ░░       ▒▒   ▓▒█░░▒▓▒ ▒ ▒   ▒ ░░   ░ ▒░▒░▒░ ░ ▒░   ░  ░ ▒▒   ▓▒█░ ▒ ░░   ░▓  ░ ▒░▒░▒░ ░ ▒░   ▒ ▒ ')
+print('  ░  ▒    ▒ ░▒░ ░ ░ ░  ░  ░  ▒   ░ ░▒ ▒░░ ░ ▒  ░ ▒ ░░ ░▒  ░ ░    ░         ▒   ▒▒ ░░░▒░ ░ ░     ░      ░ ▒ ▒░ ░  ░      ░  ▒   ▒▒ ░   ░     ▒ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░')
+print('░         ░  ░░ ░   ░   ░        ░ ░░ ░   ░ ░    ▒ ░░  ░  ░    ░           ░   ▒    ░░░ ░ ░   ░      ░ ░ ░ ▒  ░      ░     ░   ▒    ░       ▒ ░░ ░ ░ ▒     ░   ░ ░ ')
+print('░ ░       ░  ░  ░   ░  ░░ ░      ░  ░       ░  ░ ░        ░                    ░  ░   ░                  ░ ░         ░         ░  ░         ░      ░ ░           ░ ')
+print('░                       ░                                                                                                                                          ')
+print('\n\n\n')
+
+words = {}
+
+print('Enter a feature and its category (w, d, f, h): working - damage - fluids - have')
+print('\n')
+print('Formats')
+print('w - Does the _____ work properly?')
+print('d - Is the _____ broken or damage?')
+print('h - Does the car have any _____?')
+print('f - Does the car have _____ around it?')
+print('\n')
+
+
+import csv
+
+questions = {
+    "w": "Does the {word} work properly?",
+    "ww": "Do the {word} work correctly?",
+    "d": "Is the {word} broken or damaged?",
+    "dd": "Are the {word} broken or damaged?",
+    "h": "Does the car have any {word}?",
+    "f": "Does the car have {word} around it?",
+}
 
 words = {}
 
 while True:
-    # Solicitamos al usuario que ingrese la palabra y su tipo
-    entrada = input("Enter a feature and its category (w, d, f): ")
-
-     # Si el usuario decide salir, salimos del bucle infinito
+    entrada = input("word: ")
     if entrada.lower() == "stop":
         break
-
-    palabra, tipo = entrada.split()
-    words[palabra] = tipo
-
-
+    word, tipo = entrada.split()
+    words[word] = tipo
 
 with open("vehicles.csv", mode="w", newline="") as archivo_csv:
-    # Creamos un objeto writer de CSV
     writer = csv.writer(archivo_csv)
-
-    writer.writerow(["", "Type", "Expected Answer", "Critical","Exclude Random"])
-    # Recorremos las palabras del diccionario y las clasificamos según su tipo
-    for palabra, tipo in words.items():
-        if tipo == "w":
-            pregunta = f"Are the {palabra} working properly?"
-        elif tipo == "d":
-            pregunta = f"Is the {palabra} damage"
-        elif tipo == "f":
-            pregunta = f"Does the car have {palabra} around it ?"
-
-        if tipo == "w":
-            e = "Y"
-        else:
-            e = "N"
-
-        expected_answer = e
-
-        writer.writerow([pregunta, "Yes/No", expected_answer, "N", "N"])
-
-
-
+    writer.writerow(["Question", "Answer Type", "Expected Answer", "Critical", "Exclude Random"])
+    for word, tipo in words.items():
+        question = questions.get(tipo, "")
+        question = question.format(word=word)
+        expected_answer = "Y" if tipo in ("w", "h") else "N"
+        writer.writerow([question, "Yes/No", expected_answer, "N", "N"])
