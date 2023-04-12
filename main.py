598d0e91c788a309500192d58e2cfec16fc9f8e6
Programacion315 <jorgeluishuergo88@gmail.com>
2023-04-12 08:05:22 -0500

    only the feature is written

--- main.py
+++ main.py
@@ -15,17 +15,8 @@ print('\n\n\n')
 
 words = {}
 
-print('Enter a feature and its category (w, d, f, h): working - damage - fluids - have')
-print('\n')
-print('Formats')
-print('w - Does the _____ work properly?')
-print('d - Is the _____ broken or damage?')
-print('h - Does the car have any _____?')
-print('f - Does the car have _____ around it?')
-print('\n')
+print('Enter a feature and write stop when you finish')
 
-
-import csv
 
 questions = {
     "w": "Does the {word} work properly?",
@@ -36,20 +27,38 @@ questions = {
     "f": "Does the car have {word} around it?",
 }
 
-words = {}
+word_lists = {
+    "w": ["engine", "transmission", "brakes", "lights", "windows", "radio", "air conditioning", "heater", "defroster"],
+    "d": ["engine", "transmission", "brakes", "lights", "windows", "mirrors", "body"],
+    "h": ["scratches", "dents", "rust", "odor"],
+    "f": ["people", "vehicles", "obstacles"],
+}
+
+words = []
 
 while True:
     entrada = input("word: ")
     if entrada.lower() == "stop":
         break
-    word, tipo = entrada.split()
-    words[word] = tipo
+    word = entrada.lower()
+    tipo = None
+    for t, lst in word_lists.items():
+        if word in lst:
+            tipo = t
+            break
+    if tipo is None:
+        print("Word not recognized, skipping...")
+    else:
+        words.append((word, tipo))
 
 with open("vehicles.csv", mode="w", newline="") as archivo_csv:
     writer = csv.writer(archivo_csv)
     writer.writerow(["Question", "Answer Type", "Expected Answer", "Critical", "Exclude Random"])
-    for word, tipo in words.items():
-        question = questions.get(tipo, "")
-        question = question.format(word=word)
+    for word, tipo in words:
+        if tipo == "d":
+            question = "Is the {word} broken or damaged?"
+        else:
+            question = questions.get(tipo, "")
+            question = question.format(word=word)
         expected_answer = "Y" if tipo in ("w", "h") else "N"
         writer.writerow([question, "Yes/No", expected_answer, "N", "N"])
